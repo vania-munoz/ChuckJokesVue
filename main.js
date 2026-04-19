273b2c5ceda85eef4f3078e49b4d4f12a759e94f
vania-munoz <vaniailedmo@gmail.com>
2026-04-18 18:36:12 -0700

    Pagina con Vue

---
+++ main.js
@@ -0,0 +1,7 @@
+chuck: [
+  { value: "Chuck Norris can skydive into outer space." },
+  { value: "The chief export of Chuck Norris is pain." },
+  { value: "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." },
+  { value: "Time waits for no man. Unless that man is Chuck Norris." },
+  { value: "If you spell Chuck Norris in Scrabble, you win. Forever." }
+]
