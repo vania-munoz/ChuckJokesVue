469c788f9c36552d02c6a530b5985b2c9d6089b6
vania-munoz <vaniailedmo@gmail.com>
2026-04-19 19:45:54 -0700

    Activity 12

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
-chuck: [
-  { value: "Chuck Norris can skydive into outer space." },
-  { value: "The chief export of Chuck Norris is pain." },
-  { value: "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." },
-  { value: "Time waits for no man. Unless that man is Chuck Norris." },
-  { value: "If you spell Chuck Norris in Scrabble, you win. Forever." }
-]
+Vue.component('joke-card', {
+    props: ['value'],
+    template: `
+        <div class="card">
+            <h3>{{ value }}</h3>
+        </div>
+    `
+});
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Chuck Jokes Vue',
+        message: 'Enjoy!!',
+        chuck: [
+            {
+                "value": "Chuck Norris can skydive into outer space."
+            },
+            {
+                "value": "The chief export of Chuck Norris is pain."
+            },
+            {
+                "value": "Chuck Norris doesn't read books. He stares them down until he gets the information he wants."
+            },
+            {
+                "value": "Time waits for no man. Unless that man is Chuck Norris."
+            },
+            {
+                "value": "If you spell Chuck Norris in Scrabble, you win. Forever."
+            },
+        ],
+    },
+});
